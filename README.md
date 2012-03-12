c304177f9d020f9ba9b8dcc452dbc644a8fbe462
Hans-Gunther Schmidt <hans@otype.de>
2012-03-12 13:34:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# PyDoozerLib
+
+A Python client for [Doozer](https://github.com/ha/doozerd).
+
+## Latest version
+
+The latest is `0.1-SNAPSHOT`.
+
+## Installation
+
+Install via `pip`:
+
+	$ pip install git+ssh://git@github.com/cloudControl/pydoozerlib.git@0.1-SNAPSHOT
+
+### Installation error with protobuf 2.4.1
+
+There is a case when you try to install `pydoozerlib` via `python setup.py install` and run into following error:
+
+    [..]
+    Downloading http://protobuf.googlecode.com/files/protobuf-2.4.1.zip
+    Processing protobuf-2.4.1.zip
+    error: Couldn't find a setup script in /tmp/easy_install-riZxUs/protobuf-2.4.1.zip
+
+Simply install `protobuf` via `pip` manually:
+
+    $ pip install protobuf
+
+Then run `python setup.py install` again!
+
+Using the `pip install git+ssh://...` method doesn't seem to run into this issue.
+
+For more information check following issue: [Protobuf Issue #66](http://code.google.com/p/protobuf/issues/detail?id=66)
+
+## Requirements
+
+`PyDoozer` needs:
+
+- [Protobuf](http://code.google.com/p/protobuf)
+
+You will need to install these for development. Upon installation, the dependencies are automatically installed.
+
+## Todo
+
+ * currently none
+ * tests, documentation
+
+## Contributors
+
+ * Hans-Gunther Schmidt <hgs@cloudcontrol.de>
+
+## License
+
+MIT
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Copyright (c) 2012 cloudControl GmbH
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+    setup script PyDoozerLib
+
+    usage: sudo python setup.py install
+"""
+
+from distutils.core import setup
+from setuptools import find_packages
+import version
+
+DOWNLOAD_URL = 'https://github.com/downloads/cloudControl/pydoozerlib/' \
+               'pydoozerlib-{0}.tar.gz'.format(version.__version__)
+
+setup(
+    name="PyDoozer",
+    version=version.__version__,
+    description='PyDoozerLib - cloudControl\'s Doozer client library for Python.',
+    author='cloudControl Team',
+    author_email='info@cloudcontrol.de',
+    url='https://github.com/cloudControl/pydoozerlib',
+    install_requires=['protobuf'],
+    packages=find_packages(),
+    download_url=DOWNLOAD_URL,
+    license='MIT'
+)
